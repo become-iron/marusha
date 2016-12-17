2be43c33ebb705a09e8bf2e1eb40ef28b171d091
Alexander Ushakov <sasha-ushakov@mail.ru>
2016-12-17 17:46:28 +0300

    Добавлены новые слова и фразы

--- src/app/japaneseWordsAndPhrases.ts
+++ src/app/japaneseWordsAndPhrases.ts
@@ -1,100 +1,158 @@
 export interface JapaneseWordOrPhrase {
-  // kanji?: string;
-  hiragana?: string;
-  katakana?: string;
-  translation: string;
-  // translation_en?: string;
-  // transcription?: string;
-  // transcription_en?: string;
-  // pronounce_url?: string;  // ссылка на файл с произношением
+    // kanji?: string;
+    hiragana?: string;
+    katakana?: string;
+    translation: string;
+    // translation_en?: string;
+    // transcription?: string;
+    // transcription_en?: string;
+    // pronounce_url?: string;  // ссылка на файл с произношением
 }
 
 // TODO
 export function getWordsBySign(kana: string, sign: string): JapaneseWordOrPhrase[] {
-  let words: JapaneseWordOrPhrase[] = [
-    // глаголы
-    {translation: 'существовать, быть, находиться (неодушевленных предметах)', hiragana: 'ある'},
-    {translation: 'cуществовать, быть (об одушевленных предметах)', hiragana: 'いる'},
-    {translation: 'встречаться, видеться', hiragana: 'あう'},
-    {translation: 'играть, развлекаться', hiragana: 'あそぶ'},
-    {translation: 'класть во что-либо, помещать, наливать, вмещать, впускать', hiragana: 'いれる'},
-    {translation: 'вставать, просыпаться, происходить, случаться', hiragana: 'おきる'},
-    {translation: 'преподавать, учить, объяснять', hiragana: 'おしえる'},
-    {translation: 'покупать, приобретать', hiragana: 'かう'},
-    {translation: 'уходить (домой), возвращаться (домой)', hiragana: 'かえる'},
-    {translation: 'слышать, слушать, узнавать, спрашивать', hiragana: 'きく'},
-    {translation: 'надевать, носить (одежду)', hiragana: 'きる'},
-    {translation: 'приходить', hiragana: 'くる'},
-    {translation: 'закрывать, запирать, завязывать', hiragana: 'しめる'},
-    {translation: 'есть, питаться', hiragana: 'たべる'},
-    {translation: 'отличаться, "нет"', hiragana: 'ちがう'},
-    {translation: 'снимать, фотографировать', hiragana: 'とる'},
-    {translation: 'пить, глотать', hiragana: 'のむ'},
-    {translation: 'ждать, ожидать', hiragana: 'まつ'},
+    let words: JapaneseWordOrPhrase[] = [
+        // глаголы
+        {translation: 'существовать, быть, находиться (неодушевленных предметах)', hiragana: 'ある'},
+        {translation: 'cуществовать, быть (об одушевленных предметах)', hiragana: 'いる'},
+        {translation: 'встречаться, видеться', hiragana: 'あう'},
+        {translation: 'играть, развлекаться', hiragana: 'あそぶ'},
+        {translation: 'класть во что-либо, помещать, наливать, вмещать, впускать', hiragana: 'いれる'},
+        {translation: 'вставать, просыпаться, происходить, случаться', hiragana: 'おきる'},
+        {translation: 'преподавать, учить, объяснять', hiragana: 'おしえる'},
+        {translation: 'покупать, приобретать', hiragana: 'かう'},
+        {translation: 'уходить (домой), возвращаться (домой)', hiragana: 'かえる'},
+        {translation: 'слышать, слушать, узнавать, спрашивать', hiragana: 'きく'},
+        {translation: 'надевать, носить (одежду)', hiragana: 'きる'},
+        {translation: 'приходить', hiragana: 'くる'},
+        {translation: 'закрывать, запирать, завязывать', hiragana: 'しめる'},
+        {translation: 'есть, питаться', hiragana: 'たべる'},
+        {translation: 'отличаться, "нет"', hiragana: 'ちがう'},
+        {translation: 'снимать, фотографировать', hiragana: 'とる'},
+        {translation: 'пить, глотать', hiragana: 'のむ'},
+        {translation: 'ждать, ожидать', hiragana: 'まつ'},
+        {translation: 'читать', hiragana: 'よむ'},
+        {translation: 'думать', hiragana: 'おもう'},
+        {translation: 'говорить', hiragana: 'いう'},
+        {translation: 'писать', hiragana: 'かく'},
 
 
-    // существительные
-    // {translation: '', hiragana: '', kanji: ''},
-    // {translation: '', hiragana: '', kanji: ''},
-    // {translation: '', hiragana: '', kanji: ''},
+        // существительные
+        // {translation: '', hiragana: '', kanji: ''},
+        {translation: 'семья', hiragana: 'かぞく'},
+        {translation: 'мама', hiragana: 'おかあさん'},
+        {translation: 'папа', hiragana: 'おとうさん'},
+        {translation: 'мама (моя)', hiragana: 'はは'},
+        {translation: 'папа (мой)', hiragana: 'ちち'},
+        {translation: 'дочь', hiragana: 'むすめ'},
+        {translation: 'сын', hiragana: 'むすこ'},
 
-    // местоимения
-    {translation: 'кто', hiragana: 'だれ'},
-    {translation: 'когда', hiragana: 'いつ'},
-    {translation: 'где', hiragana: 'どこ'},
-    {translation: 'ты', hiragana: 'あなた'},
+        {translation: 'кошка', hiragana: 'ねこ'},
+        {translation: 'собака', hiragana: 'いぬ'},
+        {translation: 'яблоко', hiragana: 'りんご'},
+        {translation: 'вода', hiragana: 'みず'},
+        {translation: 'окно', hiragana: 'まど'},
+        {translation: 'кровать', hiragana: 'べっど'},
+        {translation: 'татами', hiragana: 'たたみ'},
 
-    // прилагательные
-    {translation: 'удивительный', hiragana: 'すごい'},
-    {translation: 'хороший', hiragana: 'いい'},
+        // местоимения
+        {translation: 'я', hiragana: 'わたし'},
+        {translation: 'кто', hiragana: 'だれ'},
+        {translation: 'когда', hiragana: 'いつ'},
+        {translation: 'где', hiragana: 'どこ'},
+        {translation: 'ты', hiragana: 'あなた'},
 
-    // катакана
-    {translation: 'туалет', katakana: 'トイレ'},
-    {translation: 'отель', katakana: 'ホテル '},
-    {translation: 'такси', katakana: 'タクシ'},
-    {translation: 'ресторан', katakana: 'レストラン'},
-    {translation: 'магазин', katakana: 'コンビニ'},
-    {translation: 'супермаркет', katakana: 'スーパー'},
-    {translation: 'кофе', katakana: 'コーヒ'},
-    {translation: 'стекло', katakana: 'ガラス'},
-    {translation: 'икра', katakana: 'イクラ'},
-  ];
-  return words.filter(word => typeof word[kana] != 'undefined' && word[kana].includes(sign));
+        // прилагательные
+        {translation: 'удивительный', hiragana: 'すごい'},
+        {translation: 'хороший', hiragana: 'いい'},
+        {translation: 'новый', hiragana: 'あたらしい'},
+        {translation: 'старый', hiragana: 'ふるい'},
+        {translation: 'большой', hiragana: 'おおきい'},
+        {translation: 'маленький', hiragana: 'ちいさい'},
+        {translation: 'длинный', hiragana: 'ながい'},
+        {translation: 'короткий', hiragana: 'みじかい'},
+        {translation: 'красный', hiragana: 'あかい'},
+        {translation: 'синий', hiragana: 'あおい'},
+        {translation: 'белый', hiragana: 'しろい'},
+        {translation: 'черный', hiragana: 'くろい'},
+
+        // катакана
+        {translation: 'туалет', katakana: 'トイレ'},
+        {translation: 'отель', katakana: 'ホテル '},
+        {translation: 'такси', katakana: 'タクシ'},
+        {translation: 'ресторан', katakana: 'レストラン'},
+        {translation: 'магазин', katakana: 'コンビニ'},
+        {translation: 'супермаркет', katakana: 'スーパー'},
+        {translation: 'кофе', katakana: 'コーヒ'},
+        {translation: 'стекло', katakana: 'ガラス'},
+        {translation: 'икра', katakana: 'イクラ'},
+        {translation: 'банан', katakana: 'バナナ'},
+        {translation: 'гамбургер', katakana: 'ハンバーガー'},
+        {translation: 'паста (макароны)', katakana: 'パスタ'},
+        {translation: 'сыр', katakana: 'チーズ'},
+        {translation: 'колбаса', katakana: 'ソーセージ'},
+        {translation: 'книга', katakana: 'ブック'},
+        {translation: 'волосы', katakana: 'ヘア'},
+        {translation: 'ручка', katakana: 'ペン'},
+
+        {translation: 'Россия', katakana: 'ロシア'},
+        {translation: 'Америка', katakana: 'アメリカ'},
+        {translation: 'Великобритания', katakana: 'イギリス'},
+    ];
+    return words.filter(word => typeof word[kana] != 'undefined' && word[kana].includes(sign));
 }
 
 export function getPhrasesBySign(kana: string, sign: string): JapaneseWordOrPhrase[] {
-  // TODO
-  let phrases: JapaneseWordOrPhrase[] = [
-    {translation: 'Добрый день', hiragana: 'こんにちは'},
-    {translation: 'Добрый вечер', hiragana: 'こんばんは'},
-    {translation: 'Доброе утро', hiragana: 'おはようございます'},
-    {translation: 'Спокойной ночи', hiragana: 'お休みなさい'},
+    // TODO
+    let phrases: JapaneseWordOrPhrase[] = [
+        {translation: 'Доброе утро', hiragana: 'おはようございます'},
+        {translation: 'Добрый день', hiragana: 'こんにちは'},
+        {translation: 'Добрый вечер', hiragana: 'こんばんは'},
+        {translation: 'Спокойной ночи', hiragana: 'お休みなさい'},
+        {translation: 'Прощай', hiragana: 'さようなら'},
+        {translation: 'Пока', hiragana: 'じゃね'},
+        {translation: 'Пока', hiragana: 'またね'},
 
-    {translation: 'Приятно познакомиться', hiragana: '初めまして。どうぞよろしく'},
-    {translation: 'Рад с вами познакомиться', hiragana: '初めまして。よろしくお願いします'},
-    {translation: 'Как поживаете?', hiragana: 'おげんきですか'},
-    {translation: 'Давно не виделись', hiragana: 'お久し振りですね'},
+        {translation: 'Добро пожаловать', hiragana: 'ようこそ'},
+        {translation: 'Добро пожаловать домой', hiragana: 'おかえり'},
+        {translation: 'С Рождеством!', hiragana: 'めりいくりすます', katakana: 'メリークリスマス'},
 
-    {translation: 'Спасибо', hiragana: 'どうも'},
-    {translation: 'Большое спасибо', hiragana: 'どうもありがとう'},
-    {translation: 'Cпасибо', hiragana: 'ありがとう'},
-    {translation: 'Спасибо (перед едой)', hiragana: 'いただきます'},
+        {translation: 'Приятно познакомиться', hiragana: 'おあいできてうれしいです'},
+        {translation: 'Как поживаете?', hiragana: 'おげんきですか'},
+        {translation: 'Как дела?', hiragana: 'ちょうしはどう？'},
 
-    {translation: 'Помогите!', hiragana: '助けて'},
-    {translation: 'Извините', hiragana: 'ごめんなさい'},
+        {translation: 'Спасибо (краткая форма)', hiragana: 'どうも'},
+        {translation: 'Большое спасибо', hiragana: 'どうもありがとう'},
+        {translation: 'Спасибо (вежливая форма)', hiragana: 'ありがとう'},
+        {translation: 'Спасибо (перед едой)', hiragana: 'いただきます'},
 
-    {translation: 'я люблю тебя', hiragana: '愛してるよ'},
-    {translation: 'я люблю тебя', hiragana: '好きだ'},
-    {translation: 'я люблю тебя', hiragana: '好きよ'},
-    {translation: 'я люблю тебя (очень люблю)', hiragana: '大好き'},
+        {translation: 'Помогите!', hiragana: '助けて'},
+        {translation: 'Простите', hiragana: 'ごめんなさい'},
+        {translation: 'Извините', hiragana: 'すみません'},
 
-    {translation: 'Алло', hiragana: 'もしもし'},
+        {translation: 'Я люблю тебя', hiragana: 'あいしてる'},
 
-    {translation: 'Я не понимаю', hiragana: 'わかりません'},
-    {translation: 'Остановись!', hiragana: 'やめて！'},
-    {translation: 'Что случилось?', hiragana: 'どうした？'},
-    {translation: 'Почему?', hiragana: 'どうして？'},
-    {translation: 'Что?', hiragana: 'なに？',},
-  ];
-  return phrases.filter(word => typeof word[kana] != 'undefined' && word[kana].includes(sign));
+        {translation: 'Алло', hiragana: 'もしもし'},
+
+        {translation: 'Я чувствую себя плохо', hiragana: 'きぶんがわるい'},
+        {translation: 'Напишите это здесь, пожалуйста', hiragana: 'ここにかいてください'},
+        {translation: 'У меня болит живот', hiragana: 'いがいたい'},
+        {translation: 'Остановись!', hiragana: 'やめて！'},
+        {translation: 'Я не знаю', hiragana: 'しりません'},
+        {translation: 'Позвони мне', hiragana: 'でんわしてね'},
+        {translation: 'Я не могу в это поверить!', hiragana: 'しんじられない！'},
+        {translation: 'Я хотел бы этот', hiragana: 'これがほしいのですが'},
+
+        {translation: 'Что вы сказали?', hiragana: 'なんていいましたか？'},
+        {translation: 'Что случилось?', hiragana: 'どうした？'},
+        {translation: 'Почему?', hiragana: 'どうして？'},
+        {translation: 'Что?', hiragana: 'なに？',},
+        {translation: 'Как тебя зовут?', hiragana: 'おなまえは？'},
+        {translation: 'Сколько тебе лет?', hiragana: 'なんさいですか？'},
+        {translation: 'Сколько это будет стоит?', hiragana: 'いくらかかりますか？'},
+        {translation: 'Могу ли я присесть здесь?', hiragana: 'ここにすわってもいいですか？'},
+        {translation: 'В самом деле?', hiragana: 'ほんと？'},
+        {translation: 'Вам помочь?', hiragana: 'ごようでしょうか？'},
+    ];
+    return phrases.filter(word => typeof word[kana] != 'undefined' && word[kana].includes(sign));
 }
